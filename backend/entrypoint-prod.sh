3798d5491ae5d202cf78eb3c4386df3dfcddeb79
James Tavares-Pitts <jtavarespitts@gmail.com>
2025-08-01 15:52:18 +0900

    Added PORT or fallback 8000 to entrypoint-prod.sh for Railway auto PORT

--- backend/entrypoint-prod.sh
+++ backend/entrypoint-prod.sh
@@ -28,4 +28,4 @@ python manage.py makemigrations
 python manage.py migrate --no-input
 
 echo "Starting Gunicorn..."
-exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn backend.wsgi:application --bind 0.0.0.0:${PORT:-8000}
